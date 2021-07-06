04bfe6e912a52eb34f268f825cc014245631c560
Andrey Bondarenko <andreyb@playtika.com>
2021-07-06 13:28:47 +0300

    Empty Spring app

---
+++ src/test/java/carshop/CarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
